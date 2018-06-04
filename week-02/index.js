36e5f1199495e494c9d1241bdce737b9f49c9c5c
Calin Segarceanu <csegarceanu@gmail.com>
2018-06-03 22:10:15 -0500

    Added the options data and wrote the raw json data in a file

--- week-02/index.js
+++ week-02/index.js
@@ -1,21 +1,27 @@
-// TODO: Read the contents of `README.md` before hacking on this file
-
+"use strict";
 // Require the file system
 const fs = require('fs');
 // Require the request-promise-native package
 const req = require('request-promise-native');
 
 const options = {
-  // TODO: Create an object literal to hit your account on the GitHub API,
-  // and pass any other information the API requires (you should not have
-  // to authenticate for this, however)
+  uri: 'https://api.github.com/users/section11',
+  headers:{
+    'User-Agent': 'Request-Promise'
+  }
 };
 
 req(options)
-  .then(
+  .then(function(data){
     // TODO: Handle the returned JSON data and write it to a file called
     // `response.json` in your `week-two/` directory
-  )
+    fs.writeFile('response.json', data, function(err){
+      if(err){
+        throw err;
+      }
+      console.log("File saved!");
+    });
+  })
   .catch(function(err) {
     console.log(err);
   });
