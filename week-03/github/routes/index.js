ef8d5999dcbac3f8033062a5fa68288fd7a9e75c
Calin Segarceanu <csegarceanu@gmail.com>
2018-06-10 22:28:24 -0500

    Requested and parse json

--- week-03/github/routes/index.js
+++ week-03/github/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise-native');
+
+var githubProfile = function (req, res, next) {
+  const options = {
+    uri: 'https://api.github.com/users/section11',
+    headers:{
+      'User-Agent': 'Request-Promise'
+    }
+  };
+  request(options).then(function(data){
+    req.profile = JSON.parse(data);
+    next();
+  });
+}
+
+router.use(githubProfile);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req);
+  res.render('index', { title: 'Github Profile', profile : req.profile });
 });
 
 module.exports = router;
